046c9f0a348f434934259ccd3fd10ef743cf0e7d
James <jturello@yahoo.com>
2016-09-13 14:19:01 -0700

    add root_path and resources: users, questions, answers, comments, votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to => 'questions#index'
+
+  resources :users
+
+  resources :questions do
+    resources :answers do
+      resources :comments do
+        resources :votes
+      end
+    end
+  end
 end
